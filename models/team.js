8c1d42b937e67e9a43bb831508b6f6ffeeb15dae
favourOkeibunor <favourokeibunor@gmail.com>
2020-05-04 08:57:59 +0100

    create new team model

--- models/team.js
+++ models/team.js
@@ -1,24 +1,12 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const team = sequelize.define('team', {
-//     name: DataTypes.STRING,
-//     abbreviation: DataTypes.STRING
-//   }, {});
-  
-//   team.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return team;
-// };
-const db = require('../models/index').sequelize;
-const Sequelize = require('../models/index').Sequelize;
-const team = db.define('team',{
-  name:{
-    type:Sequelize.STRING
-  },
-  abbreviation:{
-    type:Sequelize.STRING
-  }
-});
-
-module.exports = team;
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Team = sequelize.define('Team', {
+    id: DataTypes.STRING,
+    name: DataTypes.STRING,
+    abbreviation: DataTypes.STRING
+  }, {});
+  Team.associate = function(models) {
+    // associations can be defined here
+  };
+  return Team;
+};
