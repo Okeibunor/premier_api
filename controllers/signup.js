b79ee738a258ff2fb9b6407f566c659db4681d2e
favourOkeibunor <favourokeibunor@gmail.com>
2020-05-05 08:16:03 +0100

    Fix transaction rollback error in signup.js file

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,33 +1,45 @@
 const {v4:uuidv4} = require('uuid');
-const jwt = require('jsonwebtoken');
 const hash = require("./bcrypt").hashPassword;
 const User = require('../models/user');
-const db = require('../models/index').sequelize
 
 module.exports = async(req,res)=>{
   // define signup parameters
-  const user_id = uuidv4();
-  const user_email = req.body.email;
-  const user_password = hash(req.body.password,10);
-  // generate jwt token for user
-  const token = jwt.sign({user_id,user_email},process.env.JWT_SECRET);
+  user_email = req.body.email;
+  user_password = hash(req.body.password);
+  user_id = uuidv4();
+  User.findAll({
+    where:{
+      email:user_email
+    }}).then(result=>{
+    if(result.length >= 1){
+      res.json({"message":'Mail already has account, Login instead'})
+    }else{
+      addUser().then(res.json({message:"Account successfully created"}))
+    }
+    });  
+  }
+
+addUser = async()=>{
+  const db = require('../models/index').sequelize
   // save user data to database after creating transaction
   const t = await db.transaction();
-    try{
-      const user = await User.create({
-        id: user_id,
-        email:user_email,
-        password:user_password
-      },{transaction:t})
-      .then(res.status(201).json({"message":"New User successfully created"}))
-      .catch(err => res.json(err));
-      // commit transaction
-      await t.commit();
-    }catch(error){
-      // rollback transaction if there's an error
-      await t.rollback();
-    }
-}
+  try{
+    const user = await User.create({
+      id: user_id,
+      email:user_email,
+      password: user_password
+    },{transaction:t})
+    .then(console.log({"message":"New User successfully created"}))
+    .catch(err => console.log(err));
+    // commit transaction
+    await t.commit();
+  }
+  catch(error){
+    // rollback transaction if there's an error
+    await t.rollback();
+    }     
+  }
+      
 
 
 
